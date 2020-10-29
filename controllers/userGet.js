5d775a1871f1291a4ef4973cd76136c353754fce
Roman <zavyazkin8@gmail.com>
2020-10-30 01:24:32 +0600

    добавление времени

--- controllers/userGet.js
+++ controllers/userGet.js
@@ -3,20 +3,21 @@ const { User } = require("../model/user");
 async function getUsers(req, res) {
   const users = await User.find();
   console.log(users);
-  return res.status(200).json({ users });
+  const time = req.requestTime
+  return res.status(200).json({ time, users });
 }
 
 async function getOneUser(req, res) {
   try {
     const { id } = req.params;
-    console.log(id);
+    const time = req.requestTime
 
     const user = await User.findById(id);
-  
+  console.log(user);
     if (!user) {
       throw "Пользователь не найден";
     }
-    return res.status(200).json(user);
+    return res.status(200).json({time, user});
   } catch (e) {
     res.status(400).json(e);
   }
