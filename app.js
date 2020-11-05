c311f089cc90fb1c07eb67799ae21ddc783cb67a
Roman <zavyazkin8@gmail.com>
2020-11-05 12:51:16 +0600

    добавил cors module

--- app.js
+++ app.js
@@ -1,12 +1,14 @@
 const express = require("express");
 const app = express();
 require("dotenv").config();
+const cors = require('cors');
 
 const { router } = require("./routes/routes");
 
 const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
 
+app.use(cors());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use("/api", router);
